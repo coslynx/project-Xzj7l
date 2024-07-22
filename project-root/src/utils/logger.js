225afca8f6c3142fd54d92ec0bb4439f6a0136ea
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-22 21:36:49 +0530

    Generated: project-root/src/utils/logger.js

---
+++ project-root/src/utils/logger.js
@@ -0,0 +1,21 @@
+const winston = require('winston');
+const { token } = require('./config');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+    }),
+    new winston.transports.File({ filename: 'logs/music-bot.log' }),
+  ],
+});
+
+module.exports = logger;
